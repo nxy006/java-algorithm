cd7e3c21dcbce558292b2ef40ec04ffb38bdeb41
nxy006 <nxy006@163.com>
2021-07-08 01:43:48 +0800

    Leetcode 9. Palindrome Number

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0009/palindrome_number/MathSolution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0009/palindrome_number/MathSolution.java
@@ -3,13 +3,18 @@ package com.nxy006.project.algorithm.leetcode.p0009.palindrome_number;
 import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
 
 /**
- * TODO 待提交测试
+ * 数学算法
+ * 时间复杂度：O(log_10^{n})，空间复杂度：O(1)
+ *
+ * Runtime  8 ms    , beats 42.07 % of java submissions.
+ * Memory   38.2 MB , beats 73.53 % of java submissions.
+ * 07/08/2021 01:40
  */
 public class MathSolution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
 
-        int l = 1, r = maxSteNumber(x);
+        long l = 1, r = maxSteNumber(x);
         while(l < r) {
             if ((x / l) % 10 != (x / r) % 10) {
                 return false;
@@ -20,8 +25,8 @@ public class MathSolution {
         return true;
     }
 
-    private int maxSteNumber(int num) {
-        int res = 1;
+    private long maxSteNumber(int num) {
+        long res = 1;
         while(res * 10 <= num) {
             res *= 10;
         }
@@ -36,5 +41,6 @@ public class MathSolution {
         CaseAssertUtils.assertEquals(false, new MathSolution().isPalindrome(10));
         CaseAssertUtils.assertEquals(false, new MathSolution().isPalindrome(-101));
         CaseAssertUtils.assertEquals(true, new MathSolution().isPalindrome(0));
+        CaseAssertUtils.assertEquals(true, new MathSolution().isPalindrome(1874994781));
     }
 }
