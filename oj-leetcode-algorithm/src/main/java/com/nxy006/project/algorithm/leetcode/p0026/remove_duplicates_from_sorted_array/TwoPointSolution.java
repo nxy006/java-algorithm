d4761ca5088544094bb7f1f08273eb519da530f5
nxy006 <nxy006@163.com>
2021-07-13 22:37:38 +0800

    Leetcode 26. Remove Duplicates from Sorted Array

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0026/remove_duplicates_from_sorted_array/TwoPointSolution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0026/remove_duplicates_from_sorted_array/TwoPointSolution.java
@@ -4,9 +4,14 @@ import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
 import com.nxy006.project.alogtithm.utils.StructConvertUtils;
 
 /**
- * TODO 待提交测试
+ * 双指针解法
+ * 时间复杂度：O(n)，空间复杂度：O(1)
+ *
+ * Runtime  1 ms    , beats 48.40 % of java submissions.
+ * Memory   40.8 MB , beats 47.15 % of java submissions.
+ * 07/13/2021 22:34
  */
-public class Solution {
+public class TwoPointSolution {
     public int removeDuplicates(int[] nums) {
         int l = 0;
         for(int i = 0; i < nums.length; i++) {
@@ -22,14 +27,14 @@ public class Solution {
 
     public static void main(String[] args) {
         // 官方用例
-        caseCheck(new Solution(), "[1,2]","[1,1,2]");
-        caseCheck(new Solution(), "[0,1,2,3,4]","[0,0,1,1,1,2,2,3,3,4]");
+        caseCheck(new TwoPointSolution(), "[1,2]","[1,1,2]");
+        caseCheck(new TwoPointSolution(), "[0,1,2,3,4]","[0,0,1,1,1,2,2,3,3,4]");
         // 自定义用例
-        caseCheck(new Solution(), "[1]","[1,1,1,1,1]");
-        caseCheck(new Solution(), "[]","[]");
+        caseCheck(new TwoPointSolution(), "[1]","[1,1,1,1,1]");
+        caseCheck(new TwoPointSolution(), "[]","[]");
     }
 
-    private static void caseCheck(Solution solution, String expectedNumsStr, String numsStr) {
+    private static void caseCheck(TwoPointSolution solution, String expectedNumsStr, String numsStr) {
         int[] nums = StructConvertUtils.convertToIntArray(numsStr), expectedNums = StructConvertUtils.convertToIntArray(expectedNumsStr);
         solution.removeDuplicates(nums);
         CaseAssertUtils.assertEqualsWithLength(expectedNums, nums, expectedNums.length);
