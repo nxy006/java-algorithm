2b73801ffbac79def48ff7fe241c6283a8419a7e
nxy006 <nxy006@163.com>
2021-07-13 22:54:27 +0800

    Leetcode 31. Next Permutation

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0031/next_permutation/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0031/next_permutation/Solution.java
@@ -6,17 +6,23 @@ import com.nxy006.project.alogtithm.utils.StructConvertUtils;
 import java.util.Arrays;
 
 /**
- * TODO 待提交测试
+ * 一轮遍历解法
+ * 时间复杂度：O(n)，空间复杂度：O(1)
+ *
+ * Runtime  1 ms    , beats 43.58 % of java submissions.
+ * Memory   39.3 MB , beats 44.39 % of java submissions.
+ * 07/13/2021 22:48
  */
 public class Solution {
     public void nextPermutation(int[] nums) {
         if (nums.length <= 1) return ;
 
-        int minIndex = nums.length - 1, min = nums[minIndex];
         for(int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i+1]) {
-                swap(nums, i, nums.length - 1);
-                Arrays.sort(nums, i+1, nums.length);
+            if (nums[i] < nums[i+1]) {                              // 从后向前寻找最长逆序子数列，第一个不符合的数字（即 nums[i]）就是需要修改值的数字
+                int j = nums.length - 1;
+                while (nums[j] <= nums[i]) j--;                     // 找到逆序范围内大于该值的最小值
+                swap(nums, i, j);                                   // 交换
+                Arrays.sort(nums, i+1, nums.length);                // 逆序范围内重排序
                 return ;
             }
         }
@@ -38,7 +44,9 @@ public class Solution {
         caseCheck(new Solution(), "[1,5,1]","[1,1,5]");
         caseCheck(new Solution(), "[1]","[1]");
         // 自定义用例
-        caseCheck(new Solution(), "[1,1,2,5]","[2,5,1,1]");
+        caseCheck(new Solution(), "[5,1,1,2]","[2,5,1,1]");
+        caseCheck(new Solution(), "[3,1,2]","[2,3,1]");
+        caseCheck(new Solution(), "[5,1,1]","[1,5,1]");
     }
 
     private static void caseCheck(Solution solution, String expectedStr, String numsStr) {
