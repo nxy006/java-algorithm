c2c7976c153c16ddef7e538eb685c6107d055a8b
nxy006 <nxy006@163.com>
2021-07-15 15:16:50 +0800

    Temp Solution for Leetcode 42. Trapping Rain Water

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0042/trapping_rain_water/Solution.java
+++
@@ -1,7 +0,0 @@
-package com.nxy006.project.algorithm.leetcode.p0042.trapping_rain_water;
-
-/**
- * TODO 未完成解答
- */
-public class Solution {
-}
---
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0042/trapping_rain_water/TwoPassSolution.java
@@ -0,0 +1,39 @@
+package com.nxy006.project.algorithm.leetcode.p0042.trapping_rain_water;
+
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+
+/**
+ * TODO 未完成解答
+ */
+public class TwoPassSolution {
+    public int trap(int[] height) {
+        int[] lHeigths = new int[height.length];
+        lHeigths[0] = height[0];
+        for(int i = 1; i < height.length; i++) {
+            lHeigths[i] = Integer.max(lHeigths[i-1], height[i]);
+        }
+        int ans = 0, rHeight = height[height.length-1];
+        for(int i = height.length-1; i >= 0; i--) {
+            rHeight = Integer.max(rHeight, height[i]);
+            ans += Integer.min(lHeigths[i], rHeight) - height[i];
+        }
+        return ans;
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new TwoPassSolution(), 6, "[0,1,0,2,1,0,1,3,2,1,2,1]");
+        caseCheck(new TwoPassSolution(), 9, "[4,2,0,3,2,5]");
+        // 自定义用例
+        caseCheck(new TwoPassSolution(), 0, "[3,3,3,3,3,3,3,3]");
+        caseCheck(new TwoPassSolution(), 0, "[3,4,5,4,3]");
+        caseCheck(new TwoPassSolution(), 1, "[2,2,2,1,2,2,2]");
+    }
+
+    private static void caseCheck(TwoPassSolution solution, int expected, String numsStr) {
+        CaseAssertUtils.assertEquals(expected, solution.trap(StructConvertUtils.convertToIntArray(numsStr)));
+    }
+}
