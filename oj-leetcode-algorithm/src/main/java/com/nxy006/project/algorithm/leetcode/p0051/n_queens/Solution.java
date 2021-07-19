44508d67cdd204df1a8203cb6c06e0b2e7dbf5d0
nxy006 <nxy006@163.com>
2021-07-19 11:10:08 +0800

    Temp Solution for Leetcode 51. N-Queens, 52. N-Queens II

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0051/n_queens/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0051/n_queens/Solution.java
@@ -1,7 +1,58 @@
 package com.nxy006.project.algorithm.leetcode.p0051.n_queens;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        process(n, new boolean[n], new boolean[4*n], new boolean[4*n], 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void process(int n, boolean[] cols, boolean[] first, boolean[] second, int k, List<List<String>> res, List<String> list) {
+        if (list.size() == n) {
+            res.add(new ArrayList<>(list));
+            return ;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if (cols[i] || first[n+k-i] || second[n+i+k]) continue;
+
+            list.add(generateString(n, i));
+            cols[i] = first[n+k-i] = second[n+i+k] = true;
+
+            process(n, cols, first, second, k+1, res, list);
+
+            list.remove(list.size()-1);
+            cols[i] = first[n+k-i] = second[n+i+k] = false;
+        }
+    }
+
+    // 组装字符串
+    private String generateString(int n, int i) {
+        StringBuilder sb = new StringBuilder();
+        for(int j = 0; j < i; j++) sb.append('.');
+        sb.append('Q');
+        for(int j = i+1; j < n; j++) sb.append('.');
+        return sb.toString();
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new Solution(), "[[\".Q..\",\"...Q\",\"Q...\",\"..Q.\"],[\"..Q.\",\"Q...\",\"...Q\",\".Q..\"]]", 4);
+        caseCheck(new Solution(), "[[\"Q\"]]", 1);
+    }
+
+    private static void caseCheck(Solution solution, String expected, int n) {
+        CaseAssertUtils.assertEqualsIgnoreOrder(StructConvertUtils.convertToStringNestedList(expected), solution.solveNQueens(n));
+    }
 }
--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0052/n_queens_ii/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0052/n_queens_ii/Solution.java
@@ -1,7 +1,37 @@
 package com.nxy006.project.algorithm.leetcode.p0052.n_queens_ii;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public int totalNQueens(int n) {
+        return process(n, new boolean[n], new boolean[4*n], new boolean[4*n], 0);
+    }
+
+    private int process(int n, boolean[] cols, boolean[] first, boolean[] second, int k) {
+        if (k == n) {
+            return 1;
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if (cols[i] || first[n+k-i] || second[n+i+k]) continue;
+
+            cols[i] = first[n+k-i] = second[n+i+k] = true;
+            cnt += process(n, cols, first, second, k+1);
+            cols[i] = first[n+k-i] = second[n+i+k] = false;
+        }
+        return cnt;
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        CaseAssertUtils.assertEquals(2, new Solution().totalNQueens(4));
+        CaseAssertUtils.assertEquals(1, new Solution().totalNQueens(1));
+    }
 }
