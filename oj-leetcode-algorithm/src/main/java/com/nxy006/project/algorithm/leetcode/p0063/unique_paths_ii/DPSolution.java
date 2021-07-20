05d825e580e0234a89fdba5a5bafccdf2d4d1ec4
nxy006 <nxy006@163.com>
2021-07-20 22:55:31 +0800

    Leetcode 63. Unique Paths II

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0063/unique_paths_ii/DPSolution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0063/unique_paths_ii/DPSolution.java
@@ -4,7 +4,12 @@ import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
 import com.nxy006.project.alogtithm.utils.StructConvertUtils;
 
 /**
- * TODO 待提交测试
+ * 动态规划解法
+ * 时间复杂度：O(nm)，空间复杂度：O(nm)
+ *
+ * Runtime  0 ms    , beats 100.00 % of java submissions.
+ * Memory   38.5 MB , beats  30.78 % of java submissions.
+ * 07/20/2021 22:54
  */
 public class DPSolution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -17,7 +22,11 @@ public class DPSolution {
                     obstacleGrid[i][j] = 0;
                     continue;
                 }
-                obstacleGrid[i][j] = i == 0 || j == 0 ? 1 : obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                obstacleGrid[i][j] = (i == 0 && j == 0) ? 1 : (
+                        i == 0 ? obstacleGrid[i][j-1] :
+                        j == 0 ? obstacleGrid[i-1][j] :
+                                obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                );
             }
         }
         return obstacleGrid[m-1][n-1];
@@ -33,6 +42,7 @@ public class DPSolution {
         caseCheck(new DPSolution(), 0, "[[0,0,0],[0,1,0],[0,0,1]]");
         caseCheck(new DPSolution(), 0, "[[0,0,0],[0,1,1],[0,1,0]]");
         caseCheck(new DPSolution(), 1, "[[0,0,0],[0,1,0],[0,1,0]]");
+        caseCheck(new DPSolution(), 0, "[[0,0],[1,1],[0,0]]");
     }
 
     private static void caseCheck(DPSolution solution, int expected, String matrixStr) {
