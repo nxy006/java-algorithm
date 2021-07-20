3d55258b5e2ffa6d005270f90069263afa2cfa60
nxy006 <nxy006@163.com>
2021-07-20 10:19:10 +0800

    Temp Solution for Leetcode 58. Length of Last Word

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0058/length_of_last_word/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0058/length_of_last_word/Solution.java
@@ -1,7 +1,28 @@
 package com.nxy006.project.algorithm.leetcode.p0058.length_of_last_word;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public int lengthOfLastWord(String s) {
+        int cnt = 0;
+        for(char c : s.toCharArray()) {
+            if (c == ' ') cnt = 0;
+            else cnt++;
+        }
+        return cnt;
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        CaseAssertUtils.assertEquals(5, new Solution().lengthOfLastWord("Hello World"));
+        CaseAssertUtils.assertEquals(0, new Solution().lengthOfLastWord(" "));
+        // 自定义用例
+        CaseAssertUtils.assertEquals(3, new Solution().lengthOfLastWord("jeiafj   cefkoaf d  eic"));
+        CaseAssertUtils.assertEquals(0, new Solution().lengthOfLastWord("check then act  "));
+    }
 }
