7b4f5c3c88f5e79e1dafe30819c3c2f5c3f53123
nxy006 <nxy006@163.com>
2021-07-20 22:36:27 +0800

    Leetcode 58. Length of Last Word

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0058/length_of_last_word/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0058/length_of_last_word/Solution.java
@@ -3,14 +3,23 @@ package com.nxy006.project.algorithm.leetcode.p0058.length_of_last_word;
 import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
 
 /**
- * TODO 待提交测试
+ * 常规解法
+ *
+ * Runtime  2 ms   , beats 14.51 % of java submissions.
+ * Memory   37.2 MB , beats 73.48 % of java submissions.
+ * 07/20/2021 22:35
  */
 public class Solution {
     public int lengthOfLastWord(String s) {
         int cnt = 0;
-        for(char c : s.toCharArray()) {
-            if (c == ' ') cnt = 0;
-            else cnt++;
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                continue;
+            }
+            if (i != 0 && s.charAt(i) != ' ' && s.charAt(i-1) == ' ') {
+                cnt = 0;
+            }
+            cnt++;
         }
         return cnt;
     }
@@ -23,6 +32,7 @@ public class Solution {
         CaseAssertUtils.assertEquals(0, new Solution().lengthOfLastWord(" "));
         // 自定义用例
         CaseAssertUtils.assertEquals(3, new Solution().lengthOfLastWord("jeiafj   cefkoaf d  eic"));
-        CaseAssertUtils.assertEquals(0, new Solution().lengthOfLastWord("check then act  "));
+        CaseAssertUtils.assertEquals(3, new Solution().lengthOfLastWord("check then act  "));
+        CaseAssertUtils.assertEquals(1, new Solution().lengthOfLastWord("a "));
     }
 }
