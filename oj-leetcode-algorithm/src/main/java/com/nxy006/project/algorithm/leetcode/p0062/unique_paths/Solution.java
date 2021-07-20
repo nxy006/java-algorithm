60f58dbcaeb720be13d0b4a0937f836b7fa0e216
nxy006 <nxy006@163.com>
2021-07-20 10:52:48 +0800

    Temp Solution for Leetcode 62. Unique Paths

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0062/unique_paths/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0062/unique_paths/Solution.java
@@ -1,7 +1,28 @@
 package com.nxy006.project.algorithm.leetcode.p0062.unique_paths;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
-public class Solution {
+public class DPSolution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = i == 0 || j == 0 ? 1 : dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        CaseAssertUtils.assertEquals(28, new DPSolution().uniquePaths(3, 7));
+        CaseAssertUtils.assertEquals(3, new DPSolution().uniquePaths(3, 2));
+        CaseAssertUtils.assertEquals(28, new DPSolution().uniquePaths(7, 3));
+        CaseAssertUtils.assertEquals(6, new DPSolution().uniquePaths(3, 3));
+    }
 }
