4eb13cb39a33ff702419b68ede17a6816d42623f
nxy006 <nxy006@163.com>
2021-07-21 18:20:54 +0800

    Temp Solution for Leetcode 66. Plus One

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0066/plus_one/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0066/plus_one/Solution.java
@@ -1,7 +1,41 @@
 package com.nxy006.project.algorithm.leetcode.p0066.plus_one;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for(int i = digits.length-1; i >= 0; i--) {
+            int num = digits[i] + carry;
+            digits[i] = num % 10;
+            carry = num / 10;
+        }
+        if (carry != 0) {
+            int[] ans = new int[digits.length+1];
+            ans[0] = carry;
+            System.arraycopy(digits, 0, ans, 1, digits.length);
+            return ans;
+        } else {
+            return digits;
+        }
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new Solution(), "[1,2,4]", "[1,2,3]");
+        caseCheck(new Solution(), "[4,3,2,2]", "[4,3,2,1]");
+        caseCheck(new Solution(), "[1]", "[0]");
+        // 自定义用例
+        caseCheck(new Solution(), "[1,0,0,0]", "[9,9,9]");
+    }
+
+    private static void caseCheck(Solution solution, String expectedStr, String numsStr) {
+        CaseAssertUtils.assertEquals(StructConvertUtils.convertToIntArray(expectedStr), solution.plusOne(StructConvertUtils.convertToIntArray(numsStr)));
+    }
 }
