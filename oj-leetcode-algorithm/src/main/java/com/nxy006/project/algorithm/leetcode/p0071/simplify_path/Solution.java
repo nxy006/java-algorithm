cf4037d67d165c37785cf4fc3866bda4162e1a36
nxy006 <nxy006@163.com>
2021-07-22 10:39:07 +0800

    Temp Solution for Leetcode 71. Simplify Path

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0071/simplify_path/Solution.java
+++
@@ -1,7 +0,0 @@
-package com.nxy006.project.algorithm.leetcode.p0071.simplify_path;
-
-/**
- * TODO 未完成解答
- */
-public class Solution {
-}
---
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0071/simplify_path/StackSolution.java
@@ -0,0 +1,55 @@
+package com.nxy006.project.algorithm.leetcode.p0071.simplify_path;
+
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+
+import java.util.Stack;
+
+/**
+ * TODO 待提交测试
+ */
+public class StackSolution {
+    public String simplifyPath(String path) {
+        int start = 0;
+        Stack<String> stack = new Stack<>();
+        path = "/" + path + "/";
+        for(int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == '/') {
+                String currPath = path.substring(start, i);
+                switch (currPath) {
+                    case "/":
+                    case "/.":
+                        break;
+                    case "/..":
+                        if (!stack.isEmpty()) {
+                            stack.pop();
+                        }
+                        break;
+                    default:
+                        stack.push(currPath);
+                }
+                start = i;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        CaseAssertUtils.assertEquals("/home", new StackSolution().simplifyPath("/home/"));
+        CaseAssertUtils.assertEquals("/", new StackSolution().simplifyPath("/../"));
+        CaseAssertUtils.assertEquals("/home/foo", new StackSolution().simplifyPath("/home//foo/"));
+        CaseAssertUtils.assertEquals("/c", new StackSolution().simplifyPath("/a/./b/../../c/"));
+        // 自定义用例
+        CaseAssertUtils.assertEquals("/b", new StackSolution().simplifyPath("/a/../../../b/"));
+        CaseAssertUtils.assertEquals("/", new StackSolution().simplifyPath("/././"));
+        CaseAssertUtils.assertEquals("/a", new StackSolution().simplifyPath("/a/"));
+        CaseAssertUtils.assertEquals("/b/cdc", new StackSolution().simplifyPath("/b/cdc/"));
+    }
+}
