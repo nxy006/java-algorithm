4e0e76c9014761af3bd833f5f05af31a82bb67bb
nxy006 <nxy006@163.com>
2021-07-22 11:54:12 +0800

    Temp Solution for Leetcode 48. Rotate Image

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0048/rotate_image/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0048/rotate_image/Solution.java
@@ -1,7 +1,37 @@
 package com.nxy006.project.algorithm.leetcode.p0048.rotate_image;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        int maxI = n / 2, maxJ = m / 2 + m % 2;
+        for(int i = 0; i < maxI; i++) {
+            for(int j = 0; j < maxJ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][m-1-j];
+                matrix[n-1-i][m-1-j] = matrix[j][m-1-i];
+                matrix[j][m-1-i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new Solution(), "[[7,4,1],[8,5,2],[9,6,3]]", "[[1,2,3],[4,5,6],[7,8,9]]");
+        caseCheck(new Solution(), "[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]", "[[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]");
+        caseCheck(new Solution(), "[[1]]", "[[1]]");
+        caseCheck(new Solution(), "[[3,1],[4,2]]", "[[1,2],[3,4]]");
+    }
+
+    private static void caseCheck(Solution solution, String expected, String matrixStr) {
+        int[][] matrix = StructConvertUtils.convertToIntMatrix(matrixStr);
+        solution.rotate(matrix);
+        CaseAssertUtils.assertEquals(StructConvertUtils.convertToIntMatrix(expected), matrix);
+    }
 }
