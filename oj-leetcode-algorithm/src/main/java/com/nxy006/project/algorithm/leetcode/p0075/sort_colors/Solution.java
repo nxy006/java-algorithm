f4d35292da5ce81f8f0feab22d4eec3b500d4574
nxy006 <nxy006@163.com>
2021-07-22 19:23:06 +0800

    Temp Solution for Leetcode 75. Sort Colors

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0075/sort_colors/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0075/sort_colors/Solution.java
@@ -1,7 +1,40 @@
 package com.nxy006.project.algorithm.leetcode.p0075.sort_colors;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0, white = 0, blue = 0;
+        for(int num : nums) {
+            switch (num) {
+                case 0: red++;  break;
+                case 1: white++;break;
+                default:blue++; break;
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = red-- > 0 ? 0 : white-- > 0 ? 1 : 2;
+        }
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new Solution(), "[0,0,1,1,2,2]", "[2,0,2,1,1,0]");
+        caseCheck(new Solution(), "[0,1,2]", "[2,0,1]");
+        caseCheck(new Solution(), "[0]", "[0]");
+        caseCheck(new Solution(), "[1]", "[1]");
+    }
+
+    private static void caseCheck(Solution solution, String expectedStr, String numsStr) {
+        int[] nums = StructConvertUtils.convertToIntArray(numsStr);
+        solution.sortColors(nums);
+        CaseAssertUtils.assertEquals(StructConvertUtils.convertToIntArray(expectedStr), nums);
+    }
 }
