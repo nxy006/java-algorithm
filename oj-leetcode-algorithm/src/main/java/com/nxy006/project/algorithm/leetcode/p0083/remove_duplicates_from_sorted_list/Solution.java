119b3a461e7d0c864fa708956240f2a114087f98
nxy006 <nxy006@163.com>
2021-07-23 16:48:55 +0800

    Temp Solution for Leetcode 83. Remove Duplicates from Sorted List

--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0083/remove_duplicates_from_sorted_list/Solution.java
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0083/remove_duplicates_from_sorted_list/Solution.java
@@ -1,7 +1,38 @@
 package com.nxy006.project.algorithm.leetcode.p0083.remove_duplicates_from_sorted_list;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+import com.nxy006.project.alogtithm.utils.struct.ListNode;
+
 /**
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = new ListNode(Integer.MAX_VALUE), node = res;
+        res.next = head;
+        while(node != null) {
+            if (node.next != null && node.val == node.next.val) {
+                node.next = node.next.next;
+            }
+            node = node.next;
+        }
+        return res.next;
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new Solution(), "[1,2]", "[1,1,2]");
+        caseCheck(new Solution(), "[1,2,3]", "[1,1,2,3,3]");
+        // 自定义用例
+        caseCheck(new Solution(), "[1]", "[1,1,1,1,1]");
+        caseCheck(new Solution(), "null", "null");
+    }
+
+    private static void caseCheck(Solution solution, String expected, String listStr) {
+        CaseAssertUtils.assertEquals(StructConvertUtils.convertToListNode(expected),
+                solution.deleteDuplicates(StructConvertUtils.convertToListNode(listStr)));
+    }
 }
