335d6c9b89f2ace77475931f4cb9ba0447424a7e
nxy006 <nxy006@163.com>
2021-08-03 13:59:42 +0800

    Temp Solution for Leetcode 33. Search in Rotated Sorted Array

---
+++ oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0033/search_in_rotated_sorted_array/BinarySearchSolution.java
@@ -0,0 +1,54 @@
+package com.nxy006.project.algorithm.leetcode.p0033.search_in_rotated_sorted_array;
+
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+import com.nxy006.project.alogtithm.utils.StructConvertUtils;
+
+/**
+ * TODO 待提交测试
+ */
+public class BinarySearchSolution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length-1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid-1;
+                } else {
+                    l = mid+1;
+                }
+            } else {
+                if (nums[mid] <= target && target < nums[r]) {
+                    l = mid+1;
+                } else {
+                    r = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        // 官方用例
+        caseCheck(new BinarySearchSolution(), 4,"[4,5,6,7,0,1,2]", 0);
+        caseCheck(new BinarySearchSolution(), -1,"[4,5,6,7,0,1,2]", 3);
+        caseCheck(new BinarySearchSolution(), -1,"[1]", 0);
+        // 自定义用例
+        caseCheck(new BinarySearchSolution(), 3,"[5,6,7,0,1,2,4]", 0);
+        caseCheck(new BinarySearchSolution(), 2,"[6,7,0,1,2,4,5]", 0);
+        caseCheck(new BinarySearchSolution(), 4,"[2,4,6,7,0,1]", 0);
+        caseCheck(new BinarySearchSolution(), 3,"[4,6,7,0,1,2]", 0);
+        caseCheck(new BinarySearchSolution(), 2,"[6,7,0,1,2,4]", 0);
+        caseCheck(new BinarySearchSolution(), 1,"[6,7,0,1,2,4,5]", 7);
+        caseCheck(new BinarySearchSolution(), 3,"[6,7,0,1,2,4,5]", 1);
+        caseCheck(new BinarySearchSolution(), 4,"[6,7,0,1,2,4,5]", 2);
+    }
+
+    private static void caseCheck(BinarySearchSolution solution, int expected, String numsStr, int target) {
+        CaseAssertUtils.assertEquals(expected, solution.search(StructConvertUtils.convertToIntArray(numsStr), target));
+    }
+}
--- oj-leetcode-algorithm/src/main/java/com/nxy006/project/algorithm/leetcode/p0033/search_in_rotated_sorted_array/Solution.java
+++
@@ -1,7 +0,0 @@
-package com.nxy006.project.algorithm.leetcode.p0033.search_in_rotated_sorted_array;
-
-/**
- * TODO 未完成解答
- */
-public class Solution {
-}
