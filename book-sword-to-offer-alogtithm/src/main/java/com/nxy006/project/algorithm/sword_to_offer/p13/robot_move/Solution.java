2842fb86a5bb59b6c3d4110014fed59d2812612f
nxy006 <nxy006@163.com>
2021-08-19 19:25:38 +0800

    Temp Solution for 剑指 Offer.13：机器人的运动范围

--- book-sword-to-offer-alogtithm/src/main/java/com/nxy006/project/algorithm/sword_to_offer/p13/robot_move/Solution.java
+++ book-sword-to-offer-alogtithm/src/main/java/com/nxy006/project/algorithm/sword_to_offer/p13/robot_move/Solution.java
@@ -1,5 +1,7 @@
 package com.nxy006.project.algorithm.sword_to_offer.p13.robot_move;
 
+import com.nxy006.project.alogtithm.utils.CaseAssertUtils;
+
 /**
  * 《剑指Offer —— 名企面试官精讲典型编程题 第 2 版》
  * 面试题13：机器人的运动范围
@@ -9,7 +11,34 @@ package com.nxy006.project.algorithm.sword_to_offer.p13.robot_move;
  *       但它不能进入方格(35, 38)，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
  * 官解：https://github.com/zhedahht/CodingInterviewChinese2/tree/master/13_RobotMove
  *
- * TODO 未完成解答
+ * TODO 待提交测试
  */
 public class Solution {
+    public int movingCount(int m, int n, int k) {
+        return movingCount(m, n, k, new boolean[m][n], 0, 0);
+    }
+
+    public int movingCount(int m, int n, int k, boolean[][] exist, int i, int j) {
+        if (i >= m || j >= n || sumOfBit(i) + sumOfBit(j) > k || exist[i][j]) {
+            return 0;
+        }
+        exist[i][j] = true;
+        return movingCount(m, n, k, exist, i+1, j) + movingCount(m, n, k, exist, i, j+1) + 1;
+    }
+
+    private int sumOfBit(int n) {
+        int sum = 0;
+        while (n >= 10) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum + n;
+    }
+
+    // ---------------------------------------------------------- TEST CASE ----------------------------------------------------------- //
+
+    public static void main(String[] args) {
+        CaseAssertUtils.assertEquals(3, new Solution().movingCount(2, 3, 1));
+        CaseAssertUtils.assertEquals(1, new Solution().movingCount(3, 1, 0));
+    }
 }
